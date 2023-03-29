ce97bcbc064a45268690bacab7c5bdf34a1f1ae2
Kudana Gwatidzo <68567108+harvardkudana@users.noreply.github.com>
2023-03-29 14:25:07 -0400

    Processed data to json file
    - Extracted page number, line number, line content, and bounding box data to json file
    - Added dialog box for when file is processed successfully

--- AzureOCR.py
+++ AzureOCR.py
@@ -6,88 +6,108 @@ from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 
 import yaml
+import json
 import tkinter as tk
 import tkinter.filedialog as filedialog
 from yaml.loader import SafeLoader
 
-# Step 1: Select file using GUI
+# Global variables
+selectedFiles = []
+
+# Global functions
+def format_polygon(polygon):
+    if not polygon:
+        return "N/A"
+    return ", ".join(["[{}, {}]".format(p.x, p.y) for p in polygon])
+
+# Extract Azure credentials securely from remote config file
+with open('config.yaml', 'r') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+    azure_config = data['azure']
+SUBSCRIPTION_KEY = azure_config['SUBSCRIPTION_KEY']
+ENDPOINT = azure_config['ENDPOINT']
+azure_vision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
+document_analysis_client = DocumentAnalysisClient(endpoint=ENDPOINT, credential=AzureKeyCredential(SUBSCRIPTION_KEY))
+
+# Callback functions
+def listBoxCallback(event):
+    # fileName = os.path.basename(files[i])
+    selection = event.widget.curselection()
+    for select in selection:
+        data = event.widget.get(select)
+        selectedFiles.append(data)
+
+def processButtonCallback():
+    # Disable the process button once OCR starts
+    actionFrameBtn['state'] = 'disabled'
+    print(selectedFiles)
+    # Iterate through the selected files
+    for file in selectedFiles:
+        with open(file, 'rb') as f:
+            poller = document_analysis_client.begin_analyze_document("prebuilt-read", document=f)
+        result = poller.result()
+        line_list = []
+        for page in result.pages:
+            for line_idx, line in enumerate(page.lines):
+                line_obj = {
+                    "page_number": page.page_number,
+                    "line_number": line_idx,
+                    "line_content": line.content,
+                    "line_bounding_box": format_polygon(line.polygon)
+                }
+                line_list.append(line_obj)
+        line_list_json = json.dumps(line_list, indent=4)
+        outputFile = os.path.splitext(os.path.basename(file))[0]  + ".json"
+        with open(outputFile, "w") as o:
+            o.write(line_list_json)
+        tk.messagebox.showinfo("showinfo", "File Successfully Processed")
+    # Re-enable process button once OCR is finished
+    actionFrameBtn['state'] = 'active'
+
+def openButtonCallback():
+    files = filedialog.askopenfilenames(parent=Tk, title='Choose files for processing')
+    for i in range(len(files)):
+        fileName = files[i]
+        listFrameListBox.insert(tk.END, fileName)
+
+def resetButtonCallback():
+    listFrameListBox.delete(0, tk.END)
 
 # Initialize the GUI window
 Tk = tk.Tk()
 Tk.title('KI OCR Project Build')
-Tk.geometry('640x480')
-
-files = filedialog.askopenfilenames(parent=Tk, title='Choose files for processing')
-print(files)
+Tk.geometry('320x240')
 
 # Initialize frames for GUI, starting with the menu frame
 menuFrame = tk.Frame(Tk, bg='gray80')
 menuFrame.grid(row=0, column=0, columnspan=2)
-menuFrameFileBtn = tk.Button(menuFrame, text='Open')
+menuFrameFileBtn = tk.Button(menuFrame, text='Open', command=openButtonCallback)
 menuFrameFileBtn.grid(row=0,column=0)
-menuFrameFileBtn = tk.Button(menuFrame, text='Config')
+menuFrameFileBtn = tk.Button(menuFrame, text='Reset', command=resetButtonCallback)
 menuFrameFileBtn.grid(row=0,column=1)
 # Initialize the components for the listbox component
 listFrame = tk.Frame(Tk)
 listFrame.grid(row=1, column=0)
 listFrameScrollbar = tk.Scrollbar(listFrame, orient=tk.VERTICAL)
 listFrameScrollbar.grid(row=0, column=1)
-listFrameList = tk.Listbox(listFrame, yscrollcommand=listFrameScrollbar.set, selectmode=tk.MULTIPLE)
-listFrameList.grid(row=0, column=0)
+listFrameListBox = tk.Listbox(listFrame, yscrollcommand=listFrameScrollbar.set, selectmode=tk.MULTIPLE)
+listFrameListBox.bind("<<ListboxSelect>>", listBoxCallback)
+listFrameListBox.grid(row=0, column=0)
+listFrameScrollbar.config(command=listFrameListBox.yview)
 # Initialize action frame component
 actionFrame = tk.Frame(Tk)
 actionFrame.grid(row=1, column=1)
-actionFrameBtn = tk.Button(actionFrame, text='Process')
+actionFrameBtn = tk.Button(actionFrame, text='Process', command=processButtonCallback)
 actionFrameBtn.grid(row=0, column=0, pady=2, padx=2)
 
 Tk.rowconfigure(1, weight=1)
 Tk.columnconfigure(0, weight=1)
 Tk.columnconfigure(1, weight=1)
 
-def listFrameInit(window):
-    print("List frame init")
-
-
-for i in range(len(files)):
-    fileName = os.path.basename(files[i])
-    print(fileName)
-    listFrameList.insert(tk.END, fileName)
-    # msg = tk.Label(Tk, text=fileName)
-    # msg.grid(row=i, column=0, sticky=(tk.N, tk.S, tk.E, tk.W), pady=2, padx=2)
-    # btn = tk.Button(Tk, text='Process')
-    # btn.grid(row=i, column=1, sticky=(tk.N, tk.S, tk.E, tk.W), pady=2, padx=2)
-    # Tk.rowconfigure(i, weight=1)
-    # btn.pack()
-
-listFrameScrollbar.config(command=listFrameList.yview)
-
 Tk.mainloop()
 
-print("Please input name of file...")
-filePath = input("Path: ")
-print(filePath)
 
-# TODO, have a file selection dialog
-
-# Step 2: Extract Azure credentials securely from remote config file
-with open ('config.yaml', 'r') as f:
-    data = yaml.load(f, Loader=SafeLoader)
-    azure_config = data['azure']
-
-SUBSCRIPTION_KEY = azure_config['SUBSCRIPTION_KEY']
-ENDPOINT = azure_config['ENDPOINT']
-azure_vision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
-
-# Step 3: Upload document to azure form recognizer
-# TODO, replace this with local file selection
-# formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/read.png"
-
-document_analysis_client = DocumentAnalysisClient(endpoint=ENDPOINT, credential=AzureKeyCredential(SUBSCRIPTION_KEY))
-
-# Iterate through the selected files
-for file in files:
-    with open(file, 'rb') as f:
-        poller = document_analysis_client.begin_analyze_document("prebuilt-read", document=f)
-    result = poller.result()
-
-print("Document contains content: ", result.content)
+# for file in selectedFiles:
+    # with open(file, 'rb') as f:
+    #    poller = document_analysis_client.begin_analyze_document("prebuilt-read", document=f)
+    # # print("Document contains content: ", result.content)
